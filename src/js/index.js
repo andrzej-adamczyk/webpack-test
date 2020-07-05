6b7efd73a7af67f8d26f6b52847426a8ed55aa48
Andrzej Adamczyk <adam.griffin.usa@gmail.com>
2020-07-05 11:06:05 +0200

    Frontendowiec vs projektowanie graficzne

--- src/js/index.js
+++ src/js/index.js
@@ -28,3 +28,51 @@ document.getElementById("moment").innerHTML = stringFromNow;
 console.log("HELLO 🚀");
 
 document.cookie = name = "andrzej";
+
+console.log(localStorage.getItem("human"));
+
+// console.log(localStorage.removeItem("human"));
+
+localStorage.setItem("human", { name: "adam" });
+
+console.log(JSON.stringify({ name: "adam" }));
+
+localStorage.setItem("nowyKlucz", JSON.stringify({ name: "adam" }));
+
+const myResult = localStorage.getItem("nowyKlucz");
+
+console.log(JSON.parse(myResult));
+
+const myNewObject = JSON.parse(myResult);
+
+myNewObject.newProperty = "hi";
+
+console.log(myNewObject);
+
+sessionStorage.setItem("nowyKluczSS", JSON.stringify({ nazwa: "SS" }));
+
+const mySS = sessionStorage.getItem("nowyKluczSS");
+
+console.log(mySS);
+
+const entry = localStorage.getItem("entry");
+
+console.log(entry);
+
+let result = "";
+
+if (entry) {
+  console.log(`wartość entry: ${entry}`);
+  result = entry;
+}
+
+const entryInput = document.querySelector(".entry--js");
+entryInput.value = result;
+
+const button = document.querySelector(".button--js");
+
+console.log(button);
+
+button.addEventListener("click", () => {
+  localStorage.setItem("entry", entryInput.value);
+});
